1d114021086bc71c4036dae63d171b30781d8f9a
Carson Slater <carsonslater7@gmail.com>
2025-11-20 15:01:58 -0600

    hehe topics.html

--- MainApp/views.py
+++ MainApp/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "MainApp/index.html")
+
+
+def topics(request):
+    topics = Topic.objects.all()
+
+    context = {"T": topics}
+
+    return render(request, "MainApp/topics.html", context)
+
+
+def topic(request, topic_id):
+    t = Topic.objects.get(id=topic_id)
+
+    entries = Entry.objects.filter(topic=t).order_by["-date_added"]
+
+    context = {"topic": t, "entries": entries}
+
+    return render(request, "MainApp/")
