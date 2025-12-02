e1c03c68e112db825dd94a5da95f756efee909da
Carson Slater <carsonslater7@gmail.com>
2025-12-02 15:02:02 -0600

    edit entry  stuff and more debug

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -14,3 +14,5 @@ class EntryForm(forms.ModelForm):
         model = Entry
         fields = ["text"]
         labels = {"text": ""}
+
+        widgets = {"text": forms.Textarea(attrs={"cols": 80})}
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from MainApp.forms import *
 from .models import *
 
@@ -19,7 +19,7 @@ def topics(request):
 def topic(request, topic_id):
     t = Topic.objects.get(id=topic_id)
 
-    entries = Entry.objects.filter(topic=t).order_by["-date_added"]
+    entries = Entry.objects.filter(topic=t).order_by("-date_added")
 
     context = {"topic": t, "entries": entries}
 
@@ -41,7 +41,7 @@ def new_topic(request):
     return render(request, "MainApp/new_topic.html", context)
 
 
-def new_entry(request):
+def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != "POST":
         form = EntryForm()
@@ -49,5 +49,29 @@ def new_entry(request):
         form = EntryForm(data=request.POST)
 
         if form.is_valid():
-            new_entry = form.save(commit=false)
-            new.entry.topic = topic
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+
+            return redirect("MainApp:topic", topic_id=topic_id)
+
+    context = {"topic": topic, "form": form}
+    return render(request, "MainApp/new_entry.html", context)
+
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(data=request.POST, instance=entry)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("MainApp:topic", topic_id=topic.id)
+
+    context = {"topic": topic, "form": form, "entry": entry}
+    return render(request, "MainApp/edit_entry.html", context)
